a87fda418226f49eb232087634fa99652efd6f1f
clapitan1 <cilapitan1@gmail.com>
2023-05-25 22:15:18 -0700

    install.js

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,11 +1,33 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
+let deferredPrompt;
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    deferredPrompt = event;
+    // Show the install button
+    butInstall.style.display = 'block';
+  });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    if (deferredPrompt) {
+      // Show the installation prompt
+      deferredPrompt.prompt();
+      const result = await deferredPrompt.userChoice;
+      if (result.outcome === 'accepted') {
+        console.log('PWA installation accepted');
+      } else {
+        console.log('PWA installation dismissed');
+      }
+      deferredPrompt = null;
+      // Hide the install button
+      butInstall.style.display = 'none';
+    }
+  });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('PWA installed');
+  });
